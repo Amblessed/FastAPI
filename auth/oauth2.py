3c16941baa8a80eb7f897a198e57d2fd3ce4a86d
Bright Onwumere <brighthamza9@yahoo.com>
2022-05-09 16:09:04 +0200

    Learnt about Authentication as well as files

---
+++ auth/oauth2.py
@@ -0,0 +1,49 @@
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from db.database import get_db
+from db import db_user
+from sqlalchemy.orm.session import Session
+from fastapi import HTTPException, status
+
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# SECRET_KEY = '77407c7339a6c00544e51af1101c4abb4aea2a31157ca5f7dfd87da02a628107'
+SECRET_KEY = 'fd69379fabbee44b9077f67b49f54f02f3f7c08dc28df5fdca30e4d3b73a69a6'
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+  to_encode = data.copy()
+  if expires_delta:
+    expire = datetime.utcnow() + expires_delta
+  else:
+    expire = datetime.utcnow() + timedelta(minutes=15)
+  to_encode.update({"exp": expire})
+  encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+  return encoded_jwt
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_expection = HTTPException(
+      status_code=status.HTTP_401_UNAUTHORIZED,
+      detail = 'Could not validate credentials',
+      headers={'WWW-Authenticate': 'Bearer'}
+    )
+    try:
+      payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+      username: str = payload.get('sub')
+      if username is None:
+        raise credentials_expection
+    except JWTError:
+      raise credentials_expection
+    
+    user = db_user.get_user_by_username(db, username)
+
+    if user is None:
+      raise credentials_expection
+    return {"email": user.email, "username": user.username}
+
--- main.py
+++ main.py
@@ -2,28 +2,33 @@ from fastapi import HTTPException
 from fastapi.responses import JSONResponse, PlainTextResponse
 from fastapi import FastAPI, Request
 from exceptions import StoryException
-from router import blog_get, blog_post, user, article, product
+from router import blog_get, blog_post, user, article, product, file
+from auth import authentication
 from db import models
 from db.database import engine
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 
-#Instantiate an object of the FastAPI class
+# Instantiate an object of the FastAPI class
 app = FastAPI(swagger_ui_parameters={"defaultModelsExpandDepth": -1})
+app.include_router(authentication.router)
+app.include_router(file.router)
 app.include_router(user.router)
 app.include_router(article.router)
 app.include_router(product.router)
-app.include_router(blog_get.router)         
+app.include_router(blog_get.router)
 app.include_router(blog_post.router)
+
 
 @app.get("/hello")
 def index():
-    return {"message":"Hello World!"}
+    return {"message": "Hello World!"}
 
 
 @app.exception_handler(StoryException)
 def story_exception(request: Request, exc: StoryException):
-    return JSONResponse(status_code=418, content= {'detail': exc.name})
+    return JSONResponse(status_code=418, content={'detail': exc.name})
 
 
 # @app.exception_handler(HTTPException)
@@ -34,9 +39,11 @@ models.Base.metadata.create_all(engine)
 
 origins = ["http://localhost:3000"]
 app.add_middleware(
-    CORSMiddleware, 
-    allow_origins = origins, 
-    allow_credentials = True,
-    allow_methods = ["*"],
-    allow_headers = ["*"]
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
+
+app.mount('/files', StaticFiles(directory='files'), name='files')
